a9b09d8b90be4fcf7155657f6c33ba483a989a64
HubYura <moisiukyurii@gmail.com>
2019-12-25 01:58:18 +0200

    Update woo_odrder_DT_TM.php

--- woo_odrder_DT_TM.php
+++ woo_odrder_DT_TM.php
@@ -1,3 +1,18 @@
+function start_session_ads($key, $value) {
+	if(session_id() == '')
+     session_start(); 
+
+		$_SESSION[$key] = $value;
+
+}
+start_session_ads();
+
+add_action(‘wp_logout’, ‘end_session’);
+add_action(‘wp_login’, ‘end_session’);
+
+function end_session() {
+	session_destroy ();
+}
 add_action( 'wp_head', 'woocommerce_order_received_tag_manager' );
 
 function woocommerce_order_received_tag_manager() {
@@ -15,39 +30,47 @@ function woocommerce_order_received_tag_manager() {
         $dataLayer['transactionId']		= (string) $order->get_order_number();
         $dataLayer['transactionTotal']	= $order->get_total();
 
-	    echo "<script>dataLayer = [{'transactionId': '".$dataLayer['transactionId']."','transactionTotal': '".$dataLayer['transactionTotal']."','transactionProducts': [";
- 		
- 		foreach ($order->get_items() as $item) {
+        if ((int)$_SESSION['transactionId'] != (int)$dataLayer['transactionId']) {
 
- 			$product     = $item->get_product();
-            $product_id  = $product->get_id();
-            $product_sku = $product->get_sku();
+		    echo "<script>dataLayer = [{'transactionId': '".$dataLayer['transactionId']."','transactionTotal': '".$dataLayer['transactionTotal']."','transactionProducts': [";
+	 		
+	 		foreach ($order->get_items() as $item) {
 
-            $product_categories = get_the_terms($product_id, 'product_cat');
+	 			$product     = $item->get_product();
+	            $product_id  = $product->get_id();
+	            $product_sku = $product->get_sku();
 
-            if ((is_array($product_categories)) && (count($product_categories) > 0)) {
-                $product_cat = array_pop($product_categories);
-                $product_cat = $product_cat->name;
-            } else {
-                $product_cat = '';
-            }
+	            $product_categories = get_the_terms($product_id, 'product_cat');
 
-            $productId = $product_sku ? $product_sku : $product_id;
+	            if ((is_array($product_categories)) && (count($product_categories) > 0)) {
+	                $product_cat = array_pop($product_categories);
+	                $product_cat = $product_cat->name;
+	            } else {
+	                $product_cat = '';
+	            }
 
-            $product_price = $order->get_item_total($item);
+	            $productId = $product_sku ? $product_sku : $product_id;
 
-            $product_data  = [
-                'name'     => htmlentities($item['name'], ENT_QUOTES, 'utf-8'),
-                'sku'      => (string) $product_sku ? $product_sku : $product_id,
-                'category' => htmlentities($product_cat, ENT_QUOTES, 'utf-8'),
-                'price'    => (int) $product_price,
-                'quantity' => (int) $item['qty']
-            ];
+	            $product_price = $order->get_item_total($item);
 
-	    	echo "{'sku': '".$product_data['sku']."','name': '".$product_data['name']."','category': '".$product_data['category']."','price': '".$product_data['price']."','quantity': '".$product_data['quantity']."'}";
+	            $product_data  = [
+	                'name'     => htmlentities($item['name'], ENT_QUOTES, 'utf-8'),
+	                'sku'      => (string) $product_sku ? $product_sku : $product_id,
+	                'category' => htmlentities($product_cat, ENT_QUOTES, 'utf-8'),
+	                'price'    => (int) $product_price,
+	                'quantity' => (int) $item['qty']
+	            ];
 
- 		}
+		    	echo "{'sku': '".$product_data['sku']."','name': '".$product_data['name']."','category': '".$product_data['category']."','price': '".$product_data['price']."','quantity': '".$product_data['quantity']."'}";
 
-		echo "]}];</script>";
+	 		}
+
+			echo "]}];</script>";
+
+			start_session_ads('transactionId', $dataLayer['transactionId']);
+
+		}
+
 	}
+
 }
